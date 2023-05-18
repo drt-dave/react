b646d513444b3552dc3bc002d9ff75710b93301e
Drt-Dave <davidduartef@gmail.com>
2023-05-18 11:45:27 -0500

    sección 9- 117 useCounter - Custom Hook

---
+++ 05-hook-app/src/hooks/useCounter.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+export const useCounter = ( initialValue = 10 ) => {
+
+    const [ counter, setCounter ] = useState( initialValue )
+
+
+
+
+    return (
+        {
+            counter,
+        }
+    )
+};
+
