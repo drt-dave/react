80c17e2b2f9b79ec96b1592c748a72c27eda5b2f
Dave <davidduartef@gmail.com>
2024-06-20 22:35:51 -0500

    Sección 14: HeroesApp- Single Page Aplication - hasta 'Colocar clase de la ruta activa'

---
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -0,0 +1,25 @@
+import { Routes, Route, Navigate } from "react-router-dom"
+
+import {MarvelPage, DcPage} from "../heroes";
+import {LoginPage} from "../auth"
+import { Navbar } from "../ui"
+
+export const AppRouter = () => {
+	return (
+	<>
+	  <Navbar />
+
+	  <Routes>
+		<Route path="marvel" element={ <MarvelPage/> } />
+		<Route path="dc" element={ <DcPage/> } />
+
+		<Route path="login" element={ <LoginPage/> } />
+		<Route path="/" element={ <Navigate to="/marvel" /> } />
+	  </Routes>
+	  
+
+		
+	</>
+	)
+}
+
