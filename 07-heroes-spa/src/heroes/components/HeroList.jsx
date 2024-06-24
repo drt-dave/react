aa3137e431dd2bbf21ebf6b0ee345ef87015e95f
Dave <davidduartef@gmail.com>
2024-06-24 00:34:40 -0500

    seccion 14 HeroesApp:hasta --> leer argumentos por url

--- 07-heroes-spa/src/heroes/components/HeroList.jsx
+++ 07-heroes-spa/src/heroes/components/HeroList.jsx
@@ -1,18 +1,20 @@
 import {getHeroesByPublisher} from "../helpers"
+import {HeroCard} from "./HeroCard";
 
 export const HeroList = ({ publisher }) => {
 
   const heroes = getHeroesByPublisher( publisher );
 
-	return (
-	<>
-	  <ul>
-		{heroes.map(hero =>(
-			<li key={hero.id}>{hero.superhero}</li>
-		))}
-	  </ul>
-		
-	</>
-	)
+  return (
+	<div className="row rows-cols-1 row-cols-md-3 g-3">
+	  {heroes.map(hero =>(
+		  <HeroCard 
+		key={ hero.id }
+	  {...hero}
+	  />
+	  ))}
+	</div>
+
+  )
 }
 
