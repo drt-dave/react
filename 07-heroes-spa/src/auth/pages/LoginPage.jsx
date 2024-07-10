5be57ce9c102bbc86db441a709108f8d1caa309c
Dave <davidduartef@gmail.com>
2024-07-10 09:17:16 -0500

    Sección 15 Protecci+on de rutas en heroes-spa terminada

--- 07-heroes-spa/src/auth/pages/LoginPage.jsx
+++ 07-heroes-spa/src/auth/pages/LoginPage.jsx
@@ -1,22 +1,24 @@
+import {useContext} from "react";
 import { useNavigate } from "react-router-dom";
+import {AuthContext} from "../context";
+
+
 export const LoginPage = () => {
 
-
-
-
-
+  const { login } = useContext(AuthContext)
+  console.log(login.name)
   const navigate = useNavigate();
 
   const onLogin = () => { 
-	console.log('onLogin');
-	navigate('/dc', {
+
+	const lastPath = localStorage.getItem('lastPath') || '/';
+
+	login('Dave');
+
+	navigate(lastPath, {
 	  replace:true 
 	});
   } 
-
-
-
-
 
   return (
 	<div className="container mt-5">
--- 07-heroes-spa/src/ui/components/Navbar.jsx
+++ 07-heroes-spa/src/ui/components/Navbar.jsx
@@ -1,14 +1,18 @@
+import {useContext} from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import {AuthContext} from '../../auth/context';
 
 
 export const Navbar = () => {
 
+  const { user, logout} = useContext(AuthContext)
   const navigate = useNavigate();
 
 
 
   const onLogout = () => { 
 	console.log('onLogout');
+	logout();
 	navigate('/login', {
 	replace:true 
 	});
@@ -53,7 +57,7 @@ export const Navbar = () => {
 	  <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end ">
 		<ul className="navbar-nav ml-auto">
 		  <span className="nav-item nav-link text-primary">
-			DavoDoRoTO
+			{user?.name}
 		  </span>
 		  <button 
 				className="nav-item nav-link btn"
