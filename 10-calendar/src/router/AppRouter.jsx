8c993af6d96f4b895bcdc9c60efda076e80d77fc
Dave <davidduartef@gmail.com>
2024-12-22 01:02:03 -0500

    Creando variables de entorno

--- 10-calendar/src/router/AppRouter.jsx
+++ 10-calendar/src/router/AppRouter.jsx
@@ -1,11 +1,14 @@
 import {Navigate, Route, Routes} from "react-router-dom"
 import {LoginPage} from "../auth"
 import {CalendarPage} from "../calendar";
+import {getEnvVariables} from "../helpers";
 
 export const AppRouter = () => {
 
 
   const authStatus = 'authenticated' // 'not-authenticated';
+
+  console.log( getEnvVariables());
 
   return (
 	<Routes>
